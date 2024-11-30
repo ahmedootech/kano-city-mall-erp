a4ddce009301e098ca96de247dd04f4e632b8671
Ahmad Sheikh <ahmedootech@gmail.com>
2024-11-30 14:01:59 +0100

    build error fix

--- public/src/app/components/blogs/blog-card.tsx
+++ public/src/app/components/blogs/blog-card.tsx
@@ -1,5 +1,5 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 
 const BlogCard: React.FC<{
